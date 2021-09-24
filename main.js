93e716da873a00d444435b004e1e7152aa5d2f8c
Rémi <cabreraremi@gmail.com>
2021-09-24 14:56:54 +0200

    Correction .js div container + add some style.css

--- main.js
+++ main.js
@@ -1,50 +1,5 @@
-// const { response } = require("express");
-
-// const promise = fetch("http://localhost:3000/api/cameras");
-
-// const displayName = document.querySelector("#name");
-// const displayPrice = document.querySelector("#price");
-// const displayDescription = document.querySelector("#description");
-// const displayImagePhoto = document.querySelector("#imagePhoto");
-
-
-// promise
-// .then((response) => {
-//     const data = response.json();
-//     data.then((cameras) => {
-        
-//         for (const camera in cameras) {
-
-//         }
-
-
-        
-//         const name = cameras[0].name;
-//         const price = cameras[0].price;
-//         const description = cameras[0].description;
-//         const image = cameras[0].imageUrl;
-//         // const imagePhoto = `<img src="${image}">`;
-
-//         // console.log(image);
-
-        // displayName.innerHTML = name;
-        // displayPrice.innerHTML = price;
-        // displayDescription.innerHTML = description;
-        // // displayImagePhoto.insertAdjacentHTML("afterbegin", imagePhoto);
-
-        
-
-        // console.log(imagePhoto);
-//     })
-// })
-
-
-
-// for (let value in camera){
-//     console.log(`Affiche ${value} ${promise1[value]}`);
-// }
-
 let camerasData = [];
+const importHTML = document.querySelector("#content");
 
 const fetchCamera = async () => {
 
@@ -55,22 +10,24 @@ const fetchCamera = async () => {
     console.log(camerasData);
 };
 
-const nameDisplay = async () => {
+const cameraDisplay = async () => {
     await fetchCamera();
-    document.body.innerHTML = camerasData.map(
-        (name) =>  
+    importHTML.innerHTML = "<div id='container'>"
+    importHTML.innerHTML += camerasData.map(
+        (camera) =>  
     `
     <div class="card">
-        <img class="card-img-top" src=${name.imageUrl} alt="Photo camera">
+        <img class="card-img-top" src=${camera.imageUrl} alt="Photo camera">
         <div class="card-body">
-            <h5 class="card-title">${name.name}</h5>
-            <p class="card-text">${name.description}</p>
-            <p class="card-text">${name.price + " €"}</p>
+            <h5 class="card-title">${camera.name}</h5>
+            <p class="card-text">${camera.description}</p>
+            <p class="card-text">${camera.price + " €"}</p>
+        </div>
     </div>
     `
     )
     .join('')
-}
+    importHTML.innerHTML += "</div>"
+};
 
-nameDisplay ();
-
+cameraDisplay ();

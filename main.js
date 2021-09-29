9d70bca098fcf1f41f595e0859d8bbc9385e0918
Rémi <cabreraremi@gmail.com>
2021-09-29 09:42:28 +0200

    add products.html & products.js & _id

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let camerasData = [];
-const importHTML = document.querySelector("#content");
 
+//Création fonction en utilisant fetch pour récupérer les DATAs de l'API
 const fetchCamera = async () => {
 
     await fetch("http://localhost:3000/api/cameras")
@@ -9,24 +9,30 @@ const fetchCamera = async () => {
 
 };
 
+
+//Création variable utilisant la fonction fetch pour créer avec .map le code HTML en y implémentant les donnés
 const cameraDisplay = async () => {
+const importHTML = document.querySelector("#content");
+
     await fetchCamera();
     importHTML.innerHTML = "<div id='container'>"
     importHTML.innerHTML += camerasData.map(
         (camera) =>  
     `
     <div class="card">
-        <img class="card-img-top" src=${camera.imageUrl} alt="Photo camera">
-        <div class="card-body">
-            <h5 class="card-title">${camera.name}</h5>
-            <p class="card-text">${camera.description}</p>
-            <p class="card-text">${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(camera.price/100)}</p>
-        </div>
+        <a class="liens" href="products.html?${camera._id}">
+            <img class="card-img-top" src=${camera.imageUrl} alt="Photo camera">
+            <div class="card-body">
+                <h5 class="card-title">${camera.name}</h5>
+                <p class="card-text">${camera.description}</p>
+                <p class="card-text">${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(camera.price/100)}</p>
+            </div>
+        </a>
     </div>
     `
-    )  
+    )
     .join('')
-    importHTML.innerHTML += "</div>"
+    importHTML.innerHTML += "</div>"  
 };
 
 cameraDisplay ();
---
+++ products.js
@@ -0,0 +1,14 @@
+const selectionProduit = document.querySelector("#content");
+
+const displayProduit = 
+`
+<div class="card"
+    <img class="card-img-top" src= alt="Photo camera">
+    <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
+        <p class="card-text"></p>
+    </div>
+</div>
+`
+;

a19fb078fd049692c514e45f5b3c38bfd21796a5
Rémi <cabreraremi@gmail.com>
2021-10-07 14:40:32 +0200

    Add products in cart

--- panier.js
+++ panier.js
@@ -1,2 +1,33 @@
 let produitRegisterLS = JSON.parse(localStorage.getItem("produit"));
-console.log(produitRegisterLS);
+
+const displayProduitsPanier = document.getElementById("container");
+
+if (produitRegisterLS === null) {
+    const panierEmpty =
+        `
+<div class="Empty">
+    <p>Le panier est vide</p>
+</div>
+`;
+    displayProduitsPanier.innerHTML = panierEmpty;
+}
+else {
+    let produitsPanier = [];
+
+    for (j = 0; j < produitRegisterLS.length; j++) {
+        produitsPanier = produitsPanier +
+            `
+    <div class="insertProduct">
+        <div>Produits = ${produitRegisterLS[j].nomCamera} comprenant l'option ${produitRegisterLS[j].optionCamera}</div>
+        <div>Prix = ${produitRegisterLS[j].prix}</div>
+        <div>Supprimer le produit</div>
+    </div>
+    `;
+    }
+    if (j == produitRegisterLS.length) {
+        displayProduitsPanier.innerHTML = produitsPanier;
+    }
+
+
+
+}
--- products.js
+++ products.js
@@ -7,12 +7,12 @@ const _id = urlId.slice(1);
 //Appel du produit avec fetch et l'écrire en HTML
 const selectionProduct = document.getElementById("product");
 
-async function cameraProduct(){
+async function cameraProduct() {
 
     let reponse = await fetch(`http://localhost:3000/api/cameras/${_id}`);
     let data = await reponse.json();
-    selectionProduct.innerHTML = 
-    `
+    selectionProduct.innerHTML =
+        `
     <div class="card">
             <img class="card-img-top" src=${data.imageUrl} alt="Photo camera">
             <div class="card-body">
@@ -23,83 +23,83 @@ async function cameraProduct(){
                     <select name="option_produits" id="option_produits">
                     </select>
                 </form></br>
-                <p class="card-text">${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(data.price/100)}</p>
+                <p class="card-text">${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(data.price / 100)}</p>
                 <button id="panier" type="submit" name="ajouter-au-panier">Ajouter au panier</button>
             </div>
     </div>
     `
 
-//------Gérer les options d'achat en l'occurence les optiques pour les caméras------
-//Création de la variable recensant toutes les options d'optiques
+    //------Gérer les options d'achat en l'occurence les optiques pour les caméras------
+    //Création de la variable recensant toutes les options d'optiques
 
     const options = data.lenses;
 
-//Création d'une variable sous forme d'Array vide pour y impléter les options du produit
+    //Création d'une variable sous forme d'Array vide pour y impléter les options du produit
 
     let allOptions = [];
 
-//Création d'une boucle for pour ajouter la totalité des options du produit
+    //Création d'une boucle for pour ajouter la totalité des options du produit
 
-    for (let i = 0; i < options.length; i++){
+    for (let i = 0; i < options.length; i++) {
         allOptions += `<option value="${i}">${options[i]}</option>`
     }
 
-//Sélection la div avec l'ID pour générer le code HTML
+    //Sélection la div avec l'ID pour générer le code HTML
 
     const displayOption = document.getElementById("option_produits");
 
-//Génération du code HTML
+    //Génération du code HTML
 
     displayOption.innerHTML = allOptions;
 
-//------Gestion du panier------
-//Sélection de l'ID du bouton panier
+    //------Gestion du panier------
+    //Sélection de l'ID du bouton panier
 
     const panier = document.getElementById("panier");
 
-//Ecoute du bouton au click
+    //Ecoute du bouton au click
 
     panier.addEventListener("click", (event) => {
         event.preventDefault();
 
-    //Prendre en compte le choix de l'utilisateur
+        //Prendre en compte le choix de l'utilisateur
 
         const choiceOption = displayOption.value;
 
-    //Afficher les valeurs dans le panier
+        //Afficher les valeurs dans le panier
 
         let displayPanier = {
             nomCamera: data.name,
             optionCamera: choiceOption,
             quantite: 1,
-            prix: data.price /100 + '€',
+            prix: data.price / 100 + '€',
         }
 
-    //Paramètrage du localStorage
+        //Paramètrage du localStorage
         let produitRegisterLS = JSON.parse(localStorage.getItem("produit"));
         const resquestConfirmationPopUp = () => {
-            if(window.confirm(`${data.name} à été ajouté au panier, voulez vous aller au panier ?`)){
+            if (window.confirm(`${data.name} à été ajouté au panier, voulez vous aller au panier ?`)) {
                 window.location.href = "panier.html";
             }
-            else{
-
+            else {
+                console.log("ok");
             }
         }
-        
-        if(produitRegisterLS){
+
+        if (produitRegisterLS) {
             produitRegisterLS.push(displayPanier);
             localStorage.setItem("produit", JSON.stringify(produitRegisterLS));
             resquestConfirmationPopUp();
         }
-        else{
-            produitRegisterLS =[];
+        else {
+            produitRegisterLS = [];
             produitRegisterLS.push(displayPanier);
             localStorage.setItem("produit", JSON.stringify(produitRegisterLS));
             resquestConfirmationPopUp();
         }
     });
 }
-cameraProduct();    
+cameraProduct();
 
 
 
